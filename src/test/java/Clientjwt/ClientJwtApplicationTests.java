74a8dea09c29ab0b5a8346bf2450cadd6b2a8206
wonseokyoon <01088115030a@gmail.com>
2025-02-17 14:11:40 +0900

    first commit

---
+++ src/test/java/Clientjwt/ClientJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package Clientjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
