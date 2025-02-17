ff16b916d1edda323e37834d99a8013eed4d35d0
wonseokyoon <01088115030a@gmail.com>
2025-02-17 14:26:12 +0900

    config security

---
+++ src/main/java/Clientjwt/controller/MyController.java
@@ -0,0 +1,15 @@
+package Clientjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/my")
+    @ResponseBody
+    public String myAPI(){
+        return "My API";
+    }
+}
